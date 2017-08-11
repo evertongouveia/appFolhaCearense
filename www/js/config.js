b87dfd8bc6dd793c020043f20100360e694c0002
evertongouveia <evertongouveia2@gmail.com>
2017-08-11 09:45:30 -0300

    OneSignal

--- www/js/config.js
+++ www/js/config.js
@@ -1,15 +1,15 @@
 angular.module('WordApp.config', [])
 
-// Replace with your Wordpress Blog WP REST API url 
+// Replace with your Wordpress Blog WP REST API url
 .constant('WORDPRESS_API_URL', 'http://www.folhacearense.com.br/wp-json/wp/v2/')
 
 // Replace with your Google Project Number https://documentation.onesignal.com/v2.0/docs/android-generating-a-gcm-push-notification-key
-.constant('GOOGLE_PROJECT_NUMBER', '831781824218>')
+.constant('GOOGLE_PROJECT_NUMBER', 'onsignal-dd866')
 
 // Replace with your OneSignal AppId https://documentation.onesignal.com/docs/phonegap-sdk-installation#2-add-required-code
 .constant('ONESIGNAL_APP_ID', 'ed6fd554-44ba-4288-8ca1-6bd8dc56283e')
 
-// Change color your Ionic application to light, stable, positive, calm, balanced, energized, assertive, royal or dark 
+// Change color your Ionic application to light, stable, positive, calm, balanced, energized, assertive, royal or dark
 .constant('IONIC_APP_COLOR', 'positive')
 
 // Change Posts page template to cards, cards2, list
